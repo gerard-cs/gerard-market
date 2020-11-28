00fa1a11ab71d9dbd40cd443971c9324a25d12c5
Gerard CS <gerard.csand@gmail.com>
2020-11-28 00:04:07 -0600

    Controller de prueba inicial

---
+++ src/main/java/com/gerard/market/GerardMarketApplication.java
@@ -0,0 +1,13 @@
+package com.gerard.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerardMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerardMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gerard/market/GerardMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerard.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerardMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
